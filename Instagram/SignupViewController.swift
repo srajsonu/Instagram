f8b7040ed20a25ab6b7a7a3c1d1ea5df74aad318
srajsonu <43319626+srajsonu@users.noreply.github.com>
2019-04-29 01:16:39 +0530

    Auto layout

--- Instagram/SignupViewController.swift
+++ Instagram/SignupViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignupViewController.swift
 //  Instagram
 //
 //  Created by ARY@N on 28/04/19.
@@ -8,13 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SignupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
