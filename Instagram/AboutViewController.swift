8ef6d1cb9f0bdd0307a778fd00b372523740b1fe
srajsonu <43319626+srajsonu@users.noreply.github.com>
2019-04-29 21:48:06 +0530

    New navigation tab added

---
+++ Instagram/AboutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutViewController.swift
+//  Instagram
+//
+//  Created by ARY@N on 29/04/19.
+//  Copyright © 2019 ARYAN. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
